00757122a19b9565feaece990baf9adc00184c70
zhuNodeJs <1749238156@qq.com>
2016-10-22 14:23:30 +0800

    render的参数的学习

--- chapter 7/express_router/routes/index.js
+++ chapter 7/express_router/routes/index.js
@@ -9,4 +9,17 @@ router.get('/', function(req, res, next) {
 router.post("/", function(req, res) {
 	res.send(req.body);
 });
+
+// 7.7-7.8 实例
+router.get('/renderUse',function(req,res) {
+	// you would probably get this data from a data store
+	var user = {
+		first_name: "Lord",
+		surname: "Lucan",
+		address: "I'm not telling",
+		facebook_friends: "1356200"
+	};
+	// Note how the user object is passed as local variable to the view
+	res.render("chapterCanShu.jade",{title:"User",user:user});	
+});
 module.exports = router;
--- chapter 7/express_router/routes/users.js
+++ chapter 7/express_router/routes/users.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 7.5 在路由中添加参数的实例
+router.get('/:id', function(req, res, next){
+	res.send('show Content for user id:'+req.params.id);
+});
 
 module.exports = router;
